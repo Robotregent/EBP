2b964925921242694c43b966624a49764ed5dc9a
Fabian Schneider <fabian.schneider@hs-weingarten.de>
2011-11-19 22:55:31 +0100

    Model.data

--- AdminDialog/mamodel.cpp
+++ AdminDialog/mamodel.cpp
@@ -12,12 +12,33 @@ int MAmodel::rowCount(const QModelIndex &parent) const
 }
 int MAmodel::columnCount(const QModelIndex &parent) const
 {
-    //fünf ist nur geraten. Muss angepasst werden
-    return 5;
+    //Zwei ist nur geraten. Muss angepasst werden
+    return 2;
 }
 QVariant MAmodel::data(const QModelIndex &index, int role) const
 {
+    //Abbruch, wenn kein valider Index übergeben wurde
+    if (!index.isValid())
+             return QVariant();
+    //Abbruch, wenn Index außerhalb außerhalb der Grenzen des Models liegt
+    if (index.row() >= MAList.size()||index.row()<0)
+             return QVariant();
+    //Ausgeben der Daten
+    if (role == Qt::DisplayRole)
+    {
+        if (index.column() == 0)
+            return MAList.at(index.row())->login();
+        else if (index.column()==1)
+            return MAList.at(index.row())->name();
+        else
+            return QVariant();
 
+    }
+    else
+    {
+        return QVariant();
+    }
+    return QVariant();
 }
 QVariant MAmodel::headerData(int section, Qt::Orientation orientation, int role) const
 {
