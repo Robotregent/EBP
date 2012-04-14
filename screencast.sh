f26b5d58e4a95e237cd659f3319ecba1889b0afd
Fabian Schneider <fabian.schneider@hs-weingarten.de>
2012-04-14 12:40:38 +0200

    bugfix ereignis

---
+++ screencast.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+ffmpeg -f alsa -i plughw:0 -f x11grab -s 1366x768 -r 24 -b 100k -bf 2 -g 300 -i :0.0 -ar 22050 -ab 128k -async 50 -vol 1500 -vcodec huffyuv -aspect 1.6 -sameq screencast.avi
