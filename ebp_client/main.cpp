c916282517ab78e0a8e57e6178acdb16ea81a61b
Fabian Schneider <fabian.schneider@hs-weingarten.de>
2012-04-25 22:12:23 +0200

    Ok, wir sprechen jetzt Altenpflege. Morgen bau ich noch ne Weiche für die Behindertenhilfe Sprachdatei ein.

--- ebp_client/main.cpp
+++ ebp_client/main.cpp
@@ -11,10 +11,14 @@ int main(int argc, char *argv[])
     QTextCodec::setCodecForTr( QTextCodec::codecForName("UTF-8") );
 
     QApplication a(argc, argv);
-    QTranslator* translator = new QTranslator();
+    /*QTranslator* translator = new QTranslator();
     if (translator->load("ebp_client_Altenhilfe", ":/costumer/altenhilfe")) {
 	a.installTranslator(translator);
-    }
+    }*/
+    QTranslator translator;
+    translator.load("ebp_client_Altenhilfe");
+    a.installTranslator(&translator);
+
     MainWindow w;
     w.show();
 
