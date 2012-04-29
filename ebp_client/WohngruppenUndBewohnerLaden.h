33c618c49d5b483330ed42a0867266a2c61515f7
Tobias Himmer <provisorisch@online.de>
2012-04-29 14:49:28 +0200

    Anpassungen für Doxygen

--- ebp_client/WohngruppenUndBewohnerLaden.h
+++ ebp_client/WohngruppenUndBewohnerLaden.h
@@ -5,27 +5,23 @@
 #include <EBPdb/Wohngruppe.hxx>
 #include <QList>
 
-namespace ebp
+/**
+ * \brief Laden aller Wohnguppen. Soll QtConcurrent::run() asynchron zum Laden genutzt werden.
+ **/
+QList < QSharedPointer <ebp::Wohngruppe> > loadAllGroups(QSharedPointer<ebp::connection> aPointerToAConnection, QSharedPointer<ebp::Mitarbeiter> curMitarbeiter)
 {
-    /**
-      * \brief Laden aller Wohnguppen. Soll QtConcurrent::run() asynchron zum Laden genutzt werden.
-      **/
-    QList < QSharedPointer <ebp::Wohngruppe> > loadAllGroups(QSharedPointer<ebp::connection> aPointerToAConnection, QSharedPointer<ebp::Mitarbeiter> curMitarbeiter)
-    {
 	return curMitarbeiter->loadWohngruppen(aPointerToAConnection);
-    }
-    /**
-      * \brief Laden aller Bewohner. Soll QtConcurrent::run() asynchron zum Laden genutzt werden.
-      **/
-    QList < QSharedPointer <ebp::Bewohner> > loadAllBewohner(QSharedPointer<ebp::connection> aPointerToAConnection, QList < QSharedPointer <ebp::Wohngruppe> >allGroups )
-    {
+}
+/**
+  * \brief Laden aller Bewohner. Soll QtConcurrent::run() asynchron zum Laden genutzt werden.
+  **/
+QList < QSharedPointer <ebp::Bewohner> > loadAllBewohner(QSharedPointer<ebp::connection> aPointerToAConnection, QList < QSharedPointer <ebp::Wohngruppe> >allGroups )
+{
 	QList < QSharedPointer <ebp::Bewohner> > result= QList < QSharedPointer <ebp::Bewohner> >();
 	//return curGroup->loadBewohner(aPointerToAConnection);
 	foreach (QSharedPointer <ebp::Wohngruppe> wg, allGroups )
 	{
 	    result.append(wg->loadBewohner(aPointerToAConnection));
 	}
-
 	return result;
-    }
 }
